b514ebc3765d93aa3a4ed5e763ee29fc73e3375c
Vyachelsv Myzuka <vjacheslav.muzyka.01@privatbank.ua>
2022-01-17 21:50:13 +0200

    last commit

---
+++ HW12.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, -5, -2, 1, -4]
+
+let summ = 0
+
+for (var i = 0; i < arr.length; i++) {
+    if (arr[i] >= 0) {
+        summ += arr[i]
+    }
+}
+
+
+console.log(sum)
---
+++ HW14.js
@@ -0,0 +1,10 @@
+let num = 1000
+let iter = 0
+
+while (num >= 50) {
+    iter++
+    num /= 2
+}
+
+console.log(iter)
+console.log(num)
